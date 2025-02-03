fc1763be2f9d00abbdd4e4b15644f1bcc18e115d
Mic12321 <105850755+Mic12321@users.noreply.github.com>
2025-02-03 19:29:26 +0000

    Added reconnect WIFI logic

--- temp_humidity_reporter/temp_humidity_reporter.cpp
+++ temp_humidity_reporter/temp_humidity_reporter.cpp
@@ -1,40 +1,52 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
+#include "config.h"
 
-#define WIFI_SSID ""
-#define WIFI_PASSWORD ""
 
-int main()
-{
+bool connect_wifi() {
+    printf("Connecting to Wi-Fi...\n");
+
+    cyw43_arch_enable_sta_mode();  
+    int result = cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 10000);
+
+    if (result) {
+        printf("Wi-Fi connection failed. Error: %d\n", result);
+        return false;
+    } else {
+        printf("Connected to Wi-Fi!\n");
+        return true;
+    }
+}
+
+int main() {
     stdio_init_all();
 
-    sleep_ms(10000);
-
-    // Initialise the Wi-Fi chip
     if (cyw43_arch_init()) {
-        printf("Wi-Fi init failed\n");
+        printf("Wi-Fi initialization failed!\n");
         return -1;
     }
-    printf("Wi-Fi initialized successfully.1\n");
 
-
-    // // Enable wifi station
-    cyw43_arch_enable_sta_mode();
-
-    printf("Connecting to Wi-Fi...\n");
-    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 10000)) {
-        printf("failed to connect.\n");
-        return 1;
-    } else {
-        printf("Connected.\n");
-        // Read the ip address in a human readable way
-        uint8_t *ip_address = (uint8_t*)&(cyw43_state.netif[0].ip_addr.addr);
-        printf("IP address %d.%d.%d.%d\n", ip_address[0], ip_address[1], ip_address[2], ip_address[3]);
+    if (!connect_wifi()) {
+        printf("Initial Wi-Fi connection failed. Retrying in 10 seconds...\n");
     }
 
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+
+        if (!cyw43_tcpip_link_status(&cyw43_state, CYW43_ITF_STA)) {
+            printf("Wi-Fi disconnected! Reconnecting...\n");
+
+            // Try to reconnect
+            while (!connect_wifi()) {
+                printf("Retrying in 5 seconds...\n");
+                sleep_ms(5000);
+            }
+        } else {
+            printf("Wi-Fi connected and stable.\n");
+        }
+
+        sleep_ms(5000);
     }
+
+    return 0;
 }
